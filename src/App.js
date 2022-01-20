d26adb68664c882313443a63188f7f9045d80ee7
Dhananjay <dhananjayrakshe77@gmail.com>
2022-01-21 00:36:26 +0530

    React calculator design part complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  //createDigits() For calculator keyboard start
+  const createDigits = () => {
+    const digits = [];
+    for(let i = 1; i < 10; i++) {
+      digits.push(
+        <button>{i}</button>
+      )
+    }
+    return digits;
+  }
+  //createDigits() For calculator keyboard End
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>React Calculator</h1> */}
+      <div className="calculator">
+        <div className="display">
+          <span>(0) </span>0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+            { createDigits() }
+
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
